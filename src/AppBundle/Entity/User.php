41edf3099724df5e17be779eda13bb16a7b0d551
Digital <digital@iMac-de-admin-101.local>
2018-02-19 12:03:19 +0100

    Début security

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,11 +1,13 @@
 <?php
 namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 /**
  * @ORM\Entity
  * @ORM\Table
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -17,6 +19,16 @@ class User
      * @ORM\Column(type="string", length=255)
      */
     private $fullname;
+
+    private $roles;
+    /**
+     * @ORM\Column
+     */
+    private $password;
+    /**
+     * @ORM\Column
+     */
+    private $email;
     /**
      * @return mixed
      */
@@ -30,5 +42,38 @@ class User
     public function setFullname($fullname)
     {
         $this->fullname = $fullname;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+       return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function setUsername($email)
+    {
+        $this->email = $email;
+    }
+
+    public function eraseCredentials()
+    {
     }
 }
