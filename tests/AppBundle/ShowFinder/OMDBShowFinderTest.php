c7e0979eb05a4eabd7d176cfe819f299354160ad
Digital <digital@iMac-de-admin-101.local>
2018-04-17 11:34:54 +0200

    Tests ApiUSerPasswordAuthenticator

---
+++ tests/AppBundle/ShowFinder/OMDBShowFinderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Tests\AppBundle\ShowFinder;
+
+use AppBundle\File\FileUploader;
+use AppBundle\ShowFinder\OMDBShowFinder;
+
+
+class OMDBShowFinderTest
+{
+    public function testFindByName()
+    {
+
+    }
+
+    public function testConvertToShow()
+    {
+        $fileUploader = new FileUploader();
+
+        $apikey = 'be9bb5cf';
+    }
+
+}
